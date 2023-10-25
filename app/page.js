65c2ccdd0236e25b5653c4c61026699ceff11105
Jose Pascual <jmpascualjimenez@gmail.com>
2023-10-25 19:32:53 +0200

    feat: test

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <main className="relative w-full h-screen">
-
+      <div className="absolute inset-0 flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold text-gray-900">Hello, world!</h1>
+        <p className="text-2xl text-gray-700">Welcome to your Next.js app.</p>
+      </div>
     </main>
   )
 }
