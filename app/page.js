cef0e9f291416b155aa29bdae0046116c10f643c
Jose Pascual <jmpascualjimenez@gmail.com>
2023-10-25 19:36:41 +0200

    feat: add time

--- app/page.js
+++ app/page.js
@@ -1,10 +1,31 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Home() {
+
+  const [status, setStatus] = useState(false);
+
+  useEffect(() => {
+    const x = setTimeout(() => {
+      setStatus(true);
+
+      const y = setTimeout(() => {
+        setStatus(false);
+      }, 5000);
+
+      return () => clearTimeout(y);
+    }, 5000);
+
+    return () => clearTimeout(x);
+  })
+
   return (
     <main className="relative w-full h-screen">
-      <div className="absolute inset-0 flex flex-col items-center justify-center">
+      {status && <div className="absolute inset-0 flex flex-col items-center justify-center">
         <h1 className="text-4xl font-bold text-gray-900">Hello, world!</h1>
         <p className="text-2xl text-gray-700">Welcome to your Next.js app.</p>
-      </div>
+      </div>}
     </main>
   )
 }
